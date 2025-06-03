2ee50ff53c2d4ffcca263e4facd27d72e8c7670b
Dandara Melo <dands20melo@gmail.com>
2025-06-03 11:28:58 -0300

    :sparkles: feat: UsuarioId na classe Denúncia

---
+++ src/@types/express.d.ts
@@ -0,0 +1,8 @@
+import { Usuario } from "./Usuario";
+import { Request } from "express";
+
+declare module 'express-serve-static-core' {
+    interface Request {
+        usuario: Usuario
+    }
+}
--- src/api/controllers/denuncia/DenunciaController.ts
+++ src/api/controllers/denuncia/DenunciaController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { ObjectId } from "mongodb";
 import { Denuncia } from "../../../modules/denuncia/Denuncia";
 import { DenunciaService } from "../../../modules/denuncia/service/DenunciaService";
 
@@ -9,7 +10,23 @@ export class DenunciaController {
     try {
       const { titulo, data, status, imagem, descricao, categoria, local } = req.body;
 
-      const denuncia = new Denuncia(titulo, new Date(data), status, imagem, descricao, categoria, local);
+      const usuario = req.usuario
+
+      if(!usuario.id) {
+        return res.status(401).json({error: "usuário não autenticado"})
+      }
+
+      const usuarioId = new ObjectId(usuario.id)
+
+      const denuncia = new Denuncia(
+        titulo, 
+        new Date(data), 
+        status, 
+        descricao, 
+        categoria, 
+        local, 
+        usuarioId, 
+        imagem);
 
       const id = await service.criarDenuncia(denuncia);
       res.status(201).json({ message: "Denúncia criada com sucesso", id });
